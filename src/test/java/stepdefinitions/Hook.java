2bf6f196eddfde0e2da67ef45bf96fdbac2eb078
mehmet-alatas <mehmetqa.techproed@gmail.com>
2025-07-29 12:04:06 +0300

    e2e

--- src/test/java/stepdefinitions/Hook.java
+++ src/test/java/stepdefinitions/Hook.java
@@ -9,15 +9,10 @@ import utilities.Driver;
 
 public class Hook {
 
- @After
-    public void tearDown(Scenario scenario) throws Exception {
-            TakesScreenshot ts = (TakesScreenshot) Driver.getDriver();
-            scenario.attach(ts.getScreenshotAs(OutputType.BYTES), "image/png", "scenario_" + scenario.getName());
-    }
 
      @After
     public void tearDown2(Scenario scenario) throws Exception {
-        if (scenario.isFailed()) {//eğer scenario fail olursa calisacak
+        if (scenario.isFailed()) {
             TakesScreenshot ts = (TakesScreenshot) Driver.getDriver();
             scenario.attach(ts.getScreenshotAs(OutputType.BYTES), "image/png", "scenario_" + scenario.getName());
             Driver.closeDriver();
